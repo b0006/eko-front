a8e8645d79f3ea293b66a2863e9459175205b667
b0006 <qsefthuken@gmail.com>
2020-07-13 19:10:33 +0700

    fix(start): adaptive

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import DesktopHeader from '../../components/DesktopHeader';
 import MobileHeader from '../../components/MobileHeader';
 
+import styles from './HeaderLayout.module.scss';
+
 const HeaderLayout: React.FC = ({ children }) => {
   return (
     <>
       <DesktopHeader />
       <MobileHeader />
-      <div>{children}</div>
+      <div className={styles.content}>{children}</div>
     </>
   );
 };

e63e50b85148c28dfa2d478fbd1d689000b62d3d
b0006 <qsefthuken@gmail.com>
2020-07-14 14:25:39 +0700

    ref(header): desktop hover show/hide js

--- src/components/CategoryListHeader/index.tsx
+++ src/components/CategoryListHeader/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import classnames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,19 +9,24 @@ import { CATEGORY_LIST } from '../../mock/constants';
 import styles from './CategoryListHeader.module.scss';
 
 const CategoryListHeader: React.FC = () => {
+  const [isOpened, setIsOpened] = useState(false);
+
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.wrapper} onMouseEnter={() => setIsOpened(true)} onMouseLeave={() => setIsOpened(false)}>
       <div className={styles.title}>
         <div>
           <FontAwesomeIcon icon={faBars} size="lg" />
           <span>Категории</span>
         </div>
-        <FontAwesomeIcon className={styles.arrowIcon} icon={faChevronDown} />
+        <FontAwesomeIcon className={isOpened ? styles.arrowDown : styles.arrowUp} icon={faChevronDown} />
       </div>
-      <ul className={styles.list}>
+      <ul className={classnames({ [styles.list]: true, [styles.show]: isOpened, [styles.hide]: !isOpened })}>
         {CATEGORY_LIST.map((item) => (
           <li className={styles.listItem} key={item.value}>
-            <NavLink activeClassName={styles.listItemActive} to={`/catalog/${item.value}`}>
+            <NavLink
+              onClick={() => setIsOpened(false)}
+              activeClassName={styles.listItemActive}
+              to={`/catalog/${item.value}`}>
               {item.label}
             </NavLink>
           </li>

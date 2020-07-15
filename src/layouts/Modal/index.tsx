4a88709c3b5307f9716c8100e249a7c530377123
b0006 <qsefthuken@gmail.com>
2020-07-15 23:49:19 +0700

    fea(modal): with portal

--- src/layouts/Modal/index.tsx
+++ src/layouts/Modal/index.tsx
@@ -1,7 +1,10 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
 import classnames from 'classnames';
 
 import styles from './ModalLayout.module.scss';
+
+const modalRoot = document.getElementById('modal');
 
 interface IProps {
   isShowed: boolean;
@@ -10,12 +13,14 @@ interface IProps {
 
 const ModalLayout: React.FC<IProps> = ({ children, isShowed, hide }) => {
   const modalRef = useRef<HTMLDivElement>(null);
+  const [switchAnimation, setSwitchAnimation] = useState(false);
 
   useEffect(() => {
     const handleClick = (e: MouseEvent) => {
       if (modalRef.current && e.target instanceof Element) {
         if (!modalRef.current.contains(e.target) && isShowed) {
-          hide();
+          setSwitchAnimation(false);
+          setTimeout(() => hide(), 250);
         }
       }
     };
@@ -24,14 +29,26 @@ const ModalLayout: React.FC<IProps> = ({ children, isShowed, hide }) => {
     return () => document.removeEventListener('click', handleClick);
   }, [hide, isShowed]);
 
-  return (
-    <div className={classnames({ [styles.overlay]: true, [styles.show]: isShowed })}>
+  useEffect(() => {
+    if (isShowed) {
+      setTimeout(() => setSwitchAnimation(true), 0);
+    }
+  }, [isShowed]);
+
+  if (!isShowed || !modalRoot) {
+    return null;
+  }
+
+  const Content = (
+    <div className={classnames({ [styles.overlay]: true, [styles.show]: switchAnimation })}>
       <div ref={modalRef} className={styles.modal}>
         <input type="button" value="Закрыть" onClick={hide} />
         {children}
       </div>
     </div>
   );
+
+  return createPortal(Content, modalRoot);
 };
 
 export default ModalLayout;

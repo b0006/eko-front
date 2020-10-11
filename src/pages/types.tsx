668c0acf9fd27b3ab5171b4bdc6c8996afc963da
b0006 <qsefthuken@gmail.com>
2020-10-11 19:04:41 +0700

    ref

--- src/pages/types.tsx
+++ src/pages/types.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Route, RouteComponentProps, RouteProps } from 'react-router-dom';
 
-import Layout from '../modules/common/components/Layout';
+import { Layout } from '../modules/common/components/Layout/Loadable';
 
 interface IRouteComponentProps extends RouteProps {
   component: React.ComponentType<RouteComponentProps>;
-  isWrappedContainer?: boolean;
 }
 
 const PublicRoute: React.FC<IRouteComponentProps> = ({
   component: Component,
-  isWrappedContainer,
   ...rest
 }) => (
   <Route
